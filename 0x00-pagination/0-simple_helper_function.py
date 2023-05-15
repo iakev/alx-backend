62cf9cf5f0a6256a402eb94d0f2dfe6fde680668
iakev <kirimikmwongera@gmail.com>
2023-05-15 03:42:50 -0700

    Added a helper function that generates an index range for pagination depending on the page and page size parameters

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+"""
+Contains a helper function that returns starting and ending
+indices for particluar pagination parameters
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    takes in pagination params i.e page and page_size
+    computes the starting nd index and returns them as a tuple
+    of index range format (start_index, end_index)
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
