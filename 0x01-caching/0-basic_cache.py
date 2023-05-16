b635f42d640be17e7c1fff9c8d8f32b14790d72d
iakev <kirimikmwongera@gmail.com>
2023-05-16 04:52:22 -0700

    Modified to define base class in same module

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,7 +2,36 @@
 """
 Contains a caching system inheriting from BaseCaching
 """
-BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in your cache class")
 
 
 class BasicCache(BaseCaching):
@@ -14,7 +43,7 @@ class BasicCache(BaseCaching):
 
     def __init__(self):
         """
-        Initialize init with superclass
+        Initialize basic caching system
         """
         super().__init__()
 
