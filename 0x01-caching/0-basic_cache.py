ea31aaf9f6c628946accb92dffaae2dbb1dd1336
iakev <kirimikmwongera@gmail.com>
2023-05-16 04:48:35 -0700

    Created a class simulating a basic caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Contains a caching system inheriting from BaseCaching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Implements a caching system inheriting from BaseCaching
+    implements put and get methods for adding and accesing
+    cache items respectively
+    """
+
+    def __init__(self):
+        """
+        Initialize init with superclass
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache if key or item is None do nothing
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Gets an item by key
+        """
+        if key and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
