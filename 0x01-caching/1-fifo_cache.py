4c83aa50c67c7c71cd9507fd64fab344dceb3aa1
iakev <kirimikmwongera@gmail.com>
2023-05-16 05:08:56 -0700

    Added a class that simulates a FIFO like caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Contains a FIFO based caching system
+"""
+
+
+class BaseCaching():
+    """
+    BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """
+        Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """
+        Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        err = "put must be implemented in your cache class"
+        raise NotImplementedError(err)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        err = "get must be implemented in your cache class"
+        raise NotImplementedError(err)
+
+
+class FIFOCache(BaseCaching):
+    """
+    A FIFO Based caching system inheriting from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialize FIFOCache
+        """
+        super().__init__()
+        self.limit = BaseCaching.MAX_ITEMS
+
+    def put(self, key, item):
+        """
+        Add an item in the cache if limit passed then
+        discard First item in dictionary
+        """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > self.limit:
+            for key in self.cache_data.keys():
+                print("DISCARD: {}".format(key))
+                del self.cache_data[key]
+                break
+
+    def get(self, key):
+        """
+        Gets an item by key
+        """
+        if key and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
