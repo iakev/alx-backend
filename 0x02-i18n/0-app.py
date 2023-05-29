f86d2229840d7091d4951a9a99a8a23c2b742e94
iakev <kirimikmwongera@gmail.com>
2023-05-29 00:34:48 -0700

    Added basic app that renders a template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+basic flask app for demonstrating
+i18n 
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello() -> None:
+    return render_template('0-index.html')
