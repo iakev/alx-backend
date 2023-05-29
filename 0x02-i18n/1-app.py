24eda9e489181eddcd40665d77dce273f2992185
iakev <kirimikmwongera@gmail.com>
2023-05-29 01:29:53 -0700

    Added a flask app that configures a flask app for i18n using flask babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+basic flask app for demonstrating
+i18n
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    class to configure available languages in the app
+    """
+
+    LANGUAGES = ["en", "fr"]
+    TIMEZONE = ["UTC"]
+
+
+app.config["BABEL_DEFAULT_LOCALE"] = Config.LANGUAGES[0]
+app.config["BABEL_DEFAULT_TIMEZONE"] = Config.TIMEZONE[0]
+@app.route("/")
+def hello() -> str:
+    """
+    root route that just renders a template
+    """
+    return render_template('0-index.html')
