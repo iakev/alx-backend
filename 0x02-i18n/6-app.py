00de9c5d873a429079968c47f916036c72040070
iakev <kirimikmwongera@gmail.com>
2023-05-29 12:01:10 -0700

    Modified to correct the get_locale function to deal with possible none values

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -45,8 +45,7 @@ def get_locale() -> Optional[str]:
     languages
     """
     translation_lang = request.args.get('locale')
-    if g.user:
-        user_locale = g.user.get('locale')
+    user_locale = g.user.get('locale') if g.user else None
     header_locale = request.headers.get('locale')
     if translation_lang in app.config["LANGUAGES"]:
         return translation_lang
