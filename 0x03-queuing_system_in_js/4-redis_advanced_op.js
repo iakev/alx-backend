b702a805889f0e38023296db3ce21c72d005132d
iakev <kirimikmwongera@gmail.com>
2023-06-21 19:44:28 +0000

    Added a script that utilises hget and hgetall commands

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,17 @@
+import redis from 'redis';
+ 
+const client = redis.createClient();
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server: ', error.message);
+});
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+client.hgetall('HolbertonSchools', (err, reply) => {
+  console.log(reply);
+});
