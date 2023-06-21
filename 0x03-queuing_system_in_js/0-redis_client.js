9ca9a4719928e32f2838618644571dce71fdd6c3
iakev <kirimikmwongera@gmail.com>
2023-06-21 18:08:13 +0000

    Modified to make script functional without a function

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,12 +1,9 @@
 import { createClient } from 'redis';
 
-async function redisConnect() {
-  const client = createClient();
-  client.on('error', (error) => {
-    console.log('Redis client not connected to the server: ', error.message);
-  });
-  client.on('ready', () => {
-    console.log('Redis client connected to the server');
-  });
-}
-redisConnect();
+const client = createClient();
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server: ', error.message);
+});
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
